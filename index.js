8f4e61a018ff7638e50c02d6e4452ccf50731685
SalimSadman <salim.sadman.pranto1996@gmail.com>
2024-11-17 17:29:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var priceOfLife ="Priceless"
+console.log(priceOfLife);
+
+
+priceOfLife = 5;
+// console.log(priceOfLife);
+
+
+var name = "Monkey"
+// console.log(name);
+
+
+var hasEaten = false
+var PIE = 3.1416
+//console.log(name, priceOfLife ,typeof(hasEaten), typeof(PIE));
+
+
+var onionPrice = 15
+var eggPrice =25
+var totalPrice =onionPrice +eggPrice
+var differenceInPrice = eggPrice - onionPrice
+console.log(totalPrice, differenceInPrice);
+
+
+var banana = 10
+banana +=2
+console.log(banana)
+
+
+gold = 20
+gold *=5
+console.log(gold);
+
+
+var age = 12;
+age++
+age++
+age++
+console.log(age);
+age--
+age--
+age--
+age--
+age--
+age--
+console.log(age);
+
+
+// concatenation
+var sweet = "550.35"
+var milk = 300
+// var convertToNumber = parseInt(sweet)
+var convertToNumber = parseFloat(sweet)
+sweet = convertToNumber + milk
+// console.log(sweet);
+
+
+var today = new Date()
+console.log(today);
+console.log(today.getHours());
+console.log(typeof(today));
+console.log(today.getDate());
+
+
+var first = 0.1
+var second = 0.2
+var subTotal = first + second
+console.log(parseFloat(subTotal.toFixed(1)));
+
+
+
+
+
